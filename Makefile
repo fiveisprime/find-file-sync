b57e88cc850a4f4ce855e0c62b0565d4dadb7284
Matt Hernandez <matt@modulus.io>
2013-11-15 15:19:26 -0500

    Updated test framework.
    Updated to use mocha and should over jasmine.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ SRC = index.js spec/find-spec.js
 
 test:
 	@node node_modules/.bin/jshint $(SRC)
-	@NODE_ENV=test node node_modules/.bin/jasmine-node \
-	--verbose \
-	--captureExceptions \
+	@NODE_ENV=test node node_modules/.bin/mocha \
+	--require should \
+	--reporter spec \
 	spec
--- spec/find-spec.js
+++ spec/find-spec.js
@@ -6,28 +6,28 @@ describe('find', function() {
   it('should find files', function() {
     var found = find('spec/fixtures', 'test.js');
 
-    expect(found).not.toBeNull();
-    expect(found).toEqual(path.join('spec', 'fixtures', 'test.js'));
+    found.should.not.equal(null);
+    found.should.equal(path.join('spec', 'fixtures', 'test.js'));
   });
 
   it('should ignore excluded directories passed as an array', function() {
     var found = find('spec/fixtures', 'nested.js', ['ignored']);
 
-    expect(found).not.toBeNull();
-    expect(found).toEqual(path.join('spec', 'fixtures', 'nested', 'nested.js'));
+    found.should.not.equal(null);
+    found.should.equal(path.join('spec', 'fixtures', 'nested', 'nested.js'));
   });
 
   it('should ignore excluded directories passed as a string', function() {
     var found = find('spec/fixtures', 'nested.js', 'nested');
 
-    expect(found).not.toBeNull();
-    expect(found).toEqual(path.join('spec', 'fixtures', 'ignored', 'nested.js'));
+    found.should.not.equal(null);
+    found.should.equal(path.join('spec', 'fixtures', 'ignored', 'nested.js'));
   });
 
   it('should find files in an array', function() {
     var found = find('spec/fixtures', ['invalid', 'test.js']);
 
-    expect(found).not.toBeNull();
-    expect(found).toEqual(path.join('spec', 'fixtures', 'test.js'));
+    found.should.not.equal(null);
+    found.should.equal(path.join('spec', 'fixtures', 'test.js'));
   });
 });
